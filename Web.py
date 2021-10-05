962cd80dec26950aa0f80efc2bface3461b3ec40
WillerTrambuch <wtrambuch@gmail.com>
2021-10-05 12:29:52 -0300

    Create Web.py
    Base

---
+++ Web.py
@@ -0,0 +1,152 @@
+import streamlit as sl
+import matplotlib.pyplot as plt
+import numpy as np
+import tabula
+import pandas as pd
+import plotly as pl
+import plotly.graph_objects as go
+import plotly.express as px
+
+
+plt.rcParams['figure.figsize'] = (11,7)
+
+pdf_path_12019 = "C:\\Projetos\\Igreja\\Trimestrais\\1_2019.pdf"
+pdf_path_22019 = "C:\\Projetos\\Igreja\\Trimestrais\\2_2019.pdf"
+pdf_path_32019 = "C:\\Projetos\\Igreja\\Trimestrais\\3_2019.pdf"
+pdf_path_42019 = "C:\\Projetos\\Igreja\\Trimestrais\\4_2019.pdf"
+pdf_path_12020 = "C:\\Projetos\\Igreja\\Trimestrais\\1_2020.pdf"
+pdf_path_22020 = "C:\\Projetos\\Igreja\\Trimestrais\\2_2020.pdf"
+pdf_path_32020 = "C:\\Projetos\\Igreja\\Trimestrais\\3_2020.pdf"
+pdf_path_42020 = "C:\\Projetos\\Igreja\\Trimestrais\\4_2020.pdf"
+pdf_path_12021 = "C:\\Projetos\\Igreja\\Trimestrais\\1_2021.pdf"
+pdf_path_22021 = "C:\\Projetos\\Igreja\\Trimestrais\\2_2021.pdf"
+
+tabelas12019 = tabula.read_pdf(pdf_path_12019, pages='1-2')
+tabelas22019 = tabula.read_pdf(pdf_path_22019, pages='1-2')
+tabelas32019 = tabula.read_pdf(pdf_path_32019, pages='1-2')
+tabelas42019 = tabula.read_pdf(pdf_path_42019, pages='1-2')
+tabelas12020 = tabula.read_pdf(pdf_path_12020, pages='1-2')
+tabelas22020 = tabula.read_pdf(pdf_path_22020, pages='1-2')
+tabelas32020 = tabula.read_pdf(pdf_path_32020, pages='1-2')
+tabelas42020 = tabula.read_pdf(pdf_path_42020, pages='1-2')
+tabelas12021 = tabula.read_pdf(pdf_path_12021, pages='1-2')
+tabelas22021 = tabula.read_pdf(pdf_path_22021, pages='1-2')
+
+df1_32019 = tabelas32019[0]
+df2_32019 = tabelas32019[1]
+df3_32019 = tabelas32019[2]
+df1_32019.insert(0, "Sem_Ano", ["3_2019","3_2019","3_2019","3_2019","3_2019","3_2019","3_2019","3_2019","3_2019","3_2019","3_2019",])
+
+df1_42019 = tabelas42019[0]
+df2_42019 = tabelas42019[1]
+df3_42019 = tabelas42019[2]
+df1_42019.insert(0, "Sem_Ano", ["4_2019","4_2019","4_2019","4_2019","4_2019","4_2019","4_2019","4_2019","4_2019","4_2019","4_2019",])
+
+df1_12020 = tabelas12020[0]
+df2_12020 = tabelas12020[1]
+df3_12020 = tabelas12020[2]
+df1_12020.insert(0, "Sem_Ano", ["1_2020","1_2020","1_2020","1_2020","1_2020","1_2020","1_2020","1_2020","1_2020","1_2020","1_2020",])
+
+df1_22020 = tabelas22020[0]
+df2_22020 = tabelas22020[1]
+df3_22020 = tabelas22020[2]
+df1_22020.insert(0, "Sem_Ano", ["2_2020","2_2020","2_2020","2_2020","2_2020","2_2020","2_2020","2_2020","2_2020","2_2020","2_2020",])
+
+df1_32020 = tabelas32020[0]
+df2_32020 = tabelas32020[1]
+df3_32020 = tabelas32020[2]
+df1_32020.insert(0, "Sem_Ano", ["3_2020","3_2020","3_2020","3_2020","3_2020","3_2020","3_2020","3_2020","3_2020","3_2020","3_2020",])
+
+df1_42020 = tabelas42020[0]
+df2_42020 = tabelas42020[1]
+df3_42020 = tabelas42020[2]
+df1_42020.insert(0, "Sem_Ano", ["4_2020","4_2020","4_2020","4_2020","4_2020","4_2020","4_2020","4_2020","4_2020","4_2020","4_2020",])
+
+df1_12021 = tabelas12021[0]
+df2_12021 = tabelas12021[1]
+df3_12021 = tabelas12021[2]
+df1_12021.insert(0, "Sem_Ano", ["1_2021","1_2021","1_2021","1_2021","1_2021","1_2021","1_2021","1_2021","1_2021","1_2021","1_2021",])
+
+df1_22021 = tabelas22021[0]
+df2_22021 = tabelas22021[1]
+df3_22021 = tabelas22021[2]
+df1_22021.insert(0, "Sem_Ano", ["2_2021","2_2021","2_2021","2_2021","2_2021","2_2021","2_2021","2_2021","2_2021","2_2021","2_2021",])
+
+m = pd.merge(df1_12021, df1_22021, how = 'outer')
+m = pd.merge(m, df1_42020, how='outer')
+m = pd.merge(m, df1_32020, how='outer')
+m = pd.merge(m, df1_22020, how='outer')
+m = pd.merge(m, df1_12020, how='outer')
+m = pd.merge(m, df1_42019, how='outer')
+m = pd.merge(m, df1_32019, how='outer')
+
+m.rename(columns={'Unnamed: 0': 'Controle'}, inplace=True)
+m.rename(columns={'Ala/Ramo Real': 'AltAlg'}, inplace=True)
+m.rename(columns={'Unnamed: 1': 'Casca1'}, inplace=True)
+m.rename(columns={'Total da Estaca': 'Casca3'}, inplace=True)
+m.rename(columns={'Unnamed: 2': 'Coquei'}, inplace=True)
+m.rename(columns={'Unnamed: 3': 'Paloti'}, inplace=True)
+m.rename(columns={'Unnamed: 4': 'Toledo'}, inplace=True)
+m.rename(columns={'Unnamed: 5': 'Ignorar'}, inplace=True)
+m.rename(columns={'Unnamed: 6': 'Real_Estaca'}, inplace=True)
+m.rename(columns={'Unnamed: 7': 'Potencial_Estaca'}, inplace=True)
+m.rename(columns={'Unnamed: 8': '2021'}, inplace=True)
+m.rename(columns={'Unnamed: 9': '2020'}, inplace=True)
+m.rename(columns={'Unnamed: 10': '2016'}, inplace=True)
+m.drop('Ignorar', axis=1, inplace=True)
+m.drop(0, axis=0, inplace=True)
+m.drop(1, axis=0, inplace=True)
+m.drop(11, axis=0, inplace=True)
+m.drop(12, axis=0, inplace=True)
+
+Geral = ["Sem_Ano","Controle", "AltAlg", "Casca1", "Casca3", "Coquei", "Paloti", "Toledo", "Real_Estaca", "Potencial_Estaca"]
+Alas = ["AltAlg", "Casca1", "Casca3", "Coquei", "Paloti", "Toledo"]
+Estaca = ["Real_Estaca"]
+Alas_Estaca = ["AltAlg", "Casca1", "Casca3", "Coquei", "Paloti", "Toledo", "Real_Estaca"]
+
+Valores_alas = m.filter(items=Geral)
+Valores_estacas = m.filter(items=Geral)
+
+sl.title("Análise do Trimestral!")
+
+#Filtrar Trimestres
+base = Valores_alas.copy()
+#sl.dataframe(base)
+trimestre = base["Sem_Ano"].unique()
+tri_selecionado = sl.multiselect("Selecione o Trimestre", trimestre)
+base_new = base[base['Sem_Ano'].isin(tri_selecionado)]
+sl.dataframe(base_new)
+
+#Selecionar Gerar Gráficos
+sl.header("Gerar Gráficos")
+Controle = base["Controle"].unique()
+Controle = sl.selectbox("Selecione o Indice", Controle)
+base_controle = base[base['Controle'] == Controle]
+#sl.dataframe(base_controle)
+
+#Geração dos Gráficos
+base_controle = base_controle.sort_index(ascending=False)
+sl.dataframe(base_controle)
+Alas_Estaca = base_controle.filter(items=Alas_Estaca)
+
+# Definindo a variável do eixo x, que será o índice de uma das séries temporais
+x = base_controle["Sem_Ano"].unique()
+
+# Criando o objeto do gráfico
+fig1 = go.Figure()
+
+# Funções 'add_trace' para criar as linhas do gráfico
+for i in Alas_Estaca:
+    fig1.add_trace(go.Scatter(x=x, y=Alas_Estaca[i],
+                            mode='markers+lines',
+                            name=i))              
+                    
+# Formatando o layout do gráfico
+fig1.update_layout(title=Controle,
+                   xaxis_title='Trimestre',
+                   yaxis_title='Alas',
+                   width=1000,
+                   height=600)
+
+# Exibindo o elemento do gráfico na página                
+sl.plotly_chart(fig1)	
